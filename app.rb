3f92c8dfd45ffcb901c8525202c61b4e1fa5b2d1
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-07 13:48:23 -0500

    Prepare to scan card

--- app.rb
+++ app.rb
@@ -3,3 +3,17 @@ require 'sinatra'
 get '/' do
   erb :index, locals: { title: "Hello World" }
 end
+
+get '/login' do
+  logger.info "TODO: SCAN FOR A PIF/CAC CARD"
+  logger.info params
+
+  redirect to('/profile')
+end
+
+get '/profile' do
+  erb :profile, locals: {
+    title: "Profile Page",
+    user: { certs:[], name:"Test User", email:"abc123@gsa.gov" }
+  }
+end
