bfd247dccacdec08a52b46a28ced330ba4c84e87
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-07 20:48:56 -0500

    Fix logging

--- app.rb
+++ app.rb
@@ -2,23 +2,25 @@ require 'sinatra/base'
 #require 'pry' unless ENV.fetch("RACK_ENV") == "production"
 
 class CardReaderWebClient < Sinatra::Base
+  configure do
+    enable :logging
+  end
+
   get '/' do
-    #logger.info "SECURE? #{request.secure?}"
-    #puts "SECURE? #{request.secure?}"
     erb :index, locals: { title: "PIV/CAC Card Reader (Web Client)" }
   end
 
   get '/profile' do
-    #  logger.info "HEADERS: #{headers}"
-    #  #logger.info headers["Content-Type"]
-    #  #logger.info "PARAMS: #{params}"
-    #  #logger.info request.env["HTTP_USER_AGENT"]
-    #  logger.info request.env["SSL_CLIENT_VERIFY"]
-    #  logger.info request.env["HTTP_X_CLIENT_VERIFY"]
-    #  logger.info request.env["HTTP_X_CLIENT_S_DN"]
-    #  logger.info request.env["HTTP_X_CLIENT_I_DN"]
-    #  logger.info request.env["HTTP_X_CLIENT_SERIAL"]
-    #  logger.info request.env["HTTP_X_CLIENT_FINGERPRINT"]
+    logger.info "REQUEST SECURE? #{request.secure?}"
+    logger.info "REQUEST ENV USER_AGENT: #{request.env["HTTP_USER_AGENT"]}"
+    logger.info "REQUEST ENV SSL_CLIENT_VERIFY: #{request.env["SSL_CLIENT_VERIFY"]}"
+    logger.info "REQUEST ENV CLIENT_VERIFY: #{request.env["HTTP_X_CLIENT_VERIFY"]}"
+    logger.info "REQUEST ENV CLIENT_SUB: #{request.env["HTTP_X_CLIENT_S_DN"]}"
+    logger.info "REQUEST ENV CLIENT_ISS: #{request.env["HTTP_X_CLIENT_I_DN"]}"
+    logger.info "REQUEST ENV CLIENT_SERIAL: #{request.env["HTTP_X_CLIENT_SERIAL"]}"
+    logger.info "REQUEST ENV CLIENT_FINGERPRINT: #{request.env["HTTP_X_CLIENT_FINGERPRINT"]}"
+    logger.info "HEADERS: #{headers}" #> {"Content-Type"=>nil}
+    logger.info "PARAMS: #{params}"
 
     certs = []
     name = "Test User"
