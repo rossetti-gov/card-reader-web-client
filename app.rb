9cb1f2b65f226e3ac0782c12a76e46efa3914178
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-07 19:56:50 -0500

    Clean up

--- app.rb
+++ app.rb
@@ -1,21 +1,34 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'pry'
 
-configure { set :server, :puma }
+class CardReaderWebClient < Sinatra::Base
+  get '/' do
+    #logger.info "SECURE? #{request.secure?}"
+    puts "SECURE? #{request.secure?}"
+    erb :index, locals: { title: "PIV/CAC Card Reader (Web Client)" }
+  end
 
-get '/' do
-  erb :index, locals: { title: "Hello World" }
+  get '/profile' do
+    #  logger.info "HEADERS: #{headers}"
+    #  #logger.info headers["Content-Type"]
+    #  #logger.info "PARAMS: #{params}"
+    #  #logger.info request.env["HTTP_USER_AGENT"]
+    #  logger.info request.env["SSL_CLIENT_VERIFY"]
+    #  logger.info request.env["HTTP_X_CLIENT_VERIFY"]
+    #  logger.info request.env["HTTP_X_CLIENT_S_DN"]
+    #  logger.info request.env["HTTP_X_CLIENT_I_DN"]
+    #  logger.info request.env["HTTP_X_CLIENT_SERIAL"]
+    #  logger.info request.env["HTTP_X_CLIENT_FINGERPRINT"]
+
+    certs = []
+    name = "Test User"
+    email = "abc123@gsa.gov"
+
+    erb :profile, locals: {
+      title: "Profile Page",
+      user: { certs: certs, name: name, email: email }
+    }
+  end
 end
 
-get '/login' do
-  logger.info "TODO: SCAN FOR A PIF/CAC CARD"
-  logger.info params
-
-  redirect to('/profile')
-end
-
-get '/profile' do
-  erb :profile, locals: {
-    title: "Profile Page",
-    user: { certs:[], name:"Test User", email:"abc123@gsa.gov" }
-  }
-end
+CardReaderWebClient.run!
