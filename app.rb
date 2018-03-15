ad223fadf09499b0bca1681f8834439ee72f51e9
MJ Rossetti <michael.rossetti@gsa.gov>
2018-03-15 12:52:27 -0400

    Prepare bare-bones app for testing of web server config

--- app.rb
+++ app.rb
@@ -20,44 +20,19 @@ class CardReaderWebClient < Sinatra::Base
     logger.info "---------------"
     logger.info "PROFILE"
     logger.info "---------------"
-    #logger.info "REQUEST SECURE? #{request.secure?}"
-    #logger.info "REQUEST ENV USER_AGENT: #{request.env["HTTP_USER_AGENT"]}"
-    #logger.info "REQUEST ENV SSL_CLIENT_VERIFY: #{request.env["SSL_CLIENT_VERIFY"]}"
-    #logger.info "REQUEST ENV CLIENT_VERIFY: #{request.env["HTTP_X_CLIENT_VERIFY"]}"
-    #logger.info "REQUEST ENV CLIENT_SUB: #{request.env["HTTP_X_CLIENT_S_DN"]}"
-    #logger.info "REQUEST ENV CLIENT_ISS: #{request.env["HTTP_X_CLIENT_I_DN"]}"
-    #logger.info "REQUEST ENV CLIENT_SERIAL: #{request.env["HTTP_X_CLIENT_SERIAL"]}"
-    #logger.info "REQUEST ENV SSL_CLIENT_CERT: #{request.env["HTTP_X_CLIENT_CERT"]}"
-    #logger.info "REQUEST ENV CLIENT_FINGERPRINT: #{request.env["HTTP_X_CLIENT_FINGERPRINT"]}"
-    #logger.info "HEADERS: #{headers}" #> {"Content-Type"=>nil}
-    #logger.info "PARAMS: #{params}"
+    logger.info "REQUEST SECURE? #{request.secure?}"
+    logger.info "REQUEST ENV USER_AGENT: #{request.env["HTTP_USER_AGENT"]}"
+    logger.info "REQUEST ENV SSL_CLIENT_VERIFY: #{request.env["SSL_CLIENT_VERIFY"]}"
+    logger.info "REQUEST ENV CLIENT_VERIFY: #{request.env["HTTP_X_CLIENT_VERIFY"]}"
+    logger.info "REQUEST ENV CLIENT_SUB: #{request.env["HTTP_X_CLIENT_S_DN"]}"
+    logger.info "REQUEST ENV CLIENT_ISS: #{request.env["HTTP_X_CLIENT_I_DN"]}"
+    logger.info "REQUEST ENV CLIENT_SERIAL: #{request.env["HTTP_X_CLIENT_SERIAL"]}"
+    logger.info "REQUEST ENV SSL_CLIENT_CERT: #{request.env["HTTP_X_CLIENT_CERT"]}"
+    logger.info "REQUEST ENV CLIENT_FINGERPRINT: #{request.env["HTTP_X_CLIENT_FINGERPRINT"]}"
+    logger.info "HEADERS: #{headers}" #> {"Content-Type"=>nil}
+    logger.info "PARAMS: #{params}"
 
-    #
-    # LOCAL DEMONSTRATION/SIMULATION
-    #
-    begin
-      puts "TRYING CERT 2..."
-      raw_cert = `pkcs15-tool --read-certificate 2` #> "-----BEGIN CERTIFICATE-----\nABC...XYZ==\n-----END CERTIFICATE-----\n
-
-      if raw_cert == "" # handle "Certificate with ID '2' not found."
-        puts "TRYING CERT 1..."
-        raw_cert = `pkcs15-tool --read-certificate 1`
-      end
-
-      if raw_cert == ""
-        # if card-reader not connected, use cert from file (saves time for testing)
-        # prerequisite: save a copy of your email cert to: certs/demo_email_cert.crt
-        cert = OpenSSL::X509::Certificate.new(File.read("./certs/demo_email_cert.crt"))
-      else
-        cert = OpenSSL::X509::Certificate.new(raw_cert)
-      end
-      profile = Profile.new(cert)
-      logger.info profile
-      erb :profile, locals: {title: "PIV/CAC Card Reader (Web Client)", profile: profile}
-    rescue => e
-      logger.error "PROFILE ERROR: #{e.class} - #{e.message}"
-      redirect to('/') # TODO: session[:flash] = 'OOPS'
-    end
+    erb :profile, locals: {title: "PIV/CAC Card Reader (Web Client)"}
   end
 end
 
